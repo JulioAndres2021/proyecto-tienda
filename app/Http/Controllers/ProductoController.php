6b4a2d61b67e332cbc7630370188142f362d1156
JulioAndres2021 <andresjuliorodolfo@yahoo.com.ar>
2026-08-07 08:16:43 -0300

    feat: Agregado métodos en los controladores Categoria y Producto

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -25,4 +25,47 @@ class ProductoController extends Controller
         $producto = Producto::create($validatedData);
         return response()->json($producto, 201);
     }
+
+    //Muestra un producto específico en formato JSON
+    public function show($id) {
+        $producto = Producto::find($id);
+        //Si el producto existe, devuelve el producto en formato JSON, de lo contrario devuelve un mensaje de error
+        if ($producto) {
+            return response()->json($producto);
+        } else {
+            return response()->json(['message' => 'Producto no encontrado'], 404);
+        }
+    }
+
+    //Actualiza un Producto especifico en la base de datos
+    public function update(Request $request, $id) {
+        $producto = Producto::find($id);
+        //Si el producto existe, actualiza el producto en la base de datos, de lo contrario devuelve un mensaje de error
+        if ($producto) {
+            //Validamos los datos recibidos en la solicitud
+            $validatedData = $request->validate([
+                'nombre' => 'sometimes|required|string|max:255',
+                'descripcion' => 'nullable|string',
+                'precio' => 'sometimes|required|numeric',
+                'categoria_id' => 'sometimes|required|exists:categorias,id',
+            ]);
+
+            $producto->update($validatedData);
+            return response()->json($producto);
+        } else {
+            return response()->json(['message' => 'Producto no encontrado'], 404);
+        }
+    }
+
+    //Eliminamos un producto específico de la base de datos
+    public function delete(Request $request, $id) {
+        $producto = Producto::find($id);
+        //Si el producto existe, lo eliminamos de la base de datos, de lo contrario devuelve un mensaje de error
+        if ($producto) {
+            $producto->delete();
+            return response()->json(['message' => 'Producto eliminado correctamente']);
+        } else {
+            return response()->json(['message' => 'Producto no encontrado'], 404);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-//RUTAS DEL GRUPO API VERSION 1
+//RUTAS DEL GRUPO API VERSION 1 (Le agregamos el prefix v1 a todas las rutas)
 Route::prefix('v1')->group(function() {
 
     //PRODUCTOS
@@ -17,7 +17,12 @@ Route::prefix('v1')->group(function() {
     Route::get('/productos', [ProductoController::class, 'index'])->name('api.productos');
     //Crear un nuevo producto en la base de datos
     Route::post('/productos', [ProductoController::class, 'store'])->name('api.productos.store');
-
+    //Muestra un producto específico en formato JSON
+    Route::get('/productos/{id}', [ProductoController::class, 'show'])->name('api.productos.show');
+    //Actualiza un producto específico en la base de datos
+    Route::put('/productos/{id}', [ProductoController::class, 'update'])->name('api.productos.update');
+    //Elimina un producto específico de la base de datos
+    Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->name('api.productos.destroy');
     //CATEGORIAS
     //Obtener todas las categorías en formato JSON
     Route::get('/categorias', [CategoriaController::class, 'index'])->name('api.categorias');
