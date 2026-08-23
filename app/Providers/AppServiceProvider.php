efa694a3ab2844ebdb5274ecb2fc2a3272a22e80
JulioAndres2021 <andresjuliorodolfo@yahoo.com.ar>
2026-08-23 12:25:48 -0300

    fix: Agregado ProductoResource y saque el data de cada respuesta

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //COMPARTIMOS LA VARIABLE DE ENTORNO DE LA VERSION DEL SISTEMA CON TODAS LAS VISTAS
         View::share('appRelease', config('app.release'));
+        
+        //PARA QUE NO DEVUELVA EL DATA EN LAS RESPUESTAS JSON
+        JsonResource::withoutWrapping();
     }
 }
