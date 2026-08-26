be95cfab64cfba831f388a613aea67d8a28e9792
JulioAndres2021 <andresjuliorodolfo@yahoo.com.ar>
2026-08-26 07:18:47 -0300

    feat: relacionar usuarios con productos categorias y carritos

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,17 +2,20 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Carrito;
+use App\Models\Categoria;
+use App\Models\Producto;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -29,4 +32,42 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    //Metodos para el JWT
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
+
+    /*
+        User
+    │
+    ├── tiene muchas a categoria
+    │
+    ├── tiene muchos carritos
+    │
+    ├── tiene muchos productos
+    */
+    
+    public function productos()
+    {
+        return $this->hasMany(Producto::class, 'usuario_id');
+    }
+
+    public function categorias()
+    {
+        return $this->hasMany(Categoria::class, 'usuario_id');
+    }
+
+    public function carritos()
+    {
+        return $this->hasMany(Carrito::class, 'usuario_id');
+    }
+
+
 }
---
+++ database/migrations/2026_08_26_094123_add_usuario_id_to_categorias_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('categorias', function (Blueprint $table) {
+            $table->foreignId('usuario_id')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+
+            $table->foreignId('actualizado_por')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categorias', function (Blueprint $table) {
+            //
+        });
+    }
+};
